4c49710b6351fe4977a2c866fb7fdc5736b57986
Marcos Vinicius <marcos1305pt@gmail.com>
2018-07-28 23:36:55 -0300

    Implement method for login with JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -72,4 +72,36 @@ class AuthController extends Controller
 
     }
 
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $rules = [
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ];
+
+        $validator = Validator::make($credentials, $rules);
+
+        if($validator->fails()){
+            return response()->json(['success' => false, 'error' => $validator->messages()], 401);
+        }
+
+        $credentials['is_verified'] = 1;
+
+        try {
+            // attempt to verify the credentials and create a token fo the user
+
+            if(! $token = JWTAuth::attempt($credentials)){
+                return response()->json(['success' => false, 'error' => 'We cant find an account with this credentials. Please make sure you entered the right information and you have verified your email address.'], 404);
+
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'Failed to login, please try again'], 500);
+        }
+
+        //all good so return the token
+        return response()->json(['success' => true, 'data' => ['token' => $token]], 200);
+    }
+
 }
